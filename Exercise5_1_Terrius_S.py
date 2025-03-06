c3126664a585959ee735ade665aee0f4cb3c0a2d
Kidchumaiork <glaosad5@gmail.com>
2025-03-06 16:42:22 +0700

    Submit Exercise5_1

---
+++ Exercise5_1_Terrius_S.py
@@ -0,0 +1,7 @@
+numX = int(input("Input first number: "))
+numY = int(input("Input second number: "))
+
+print(numX, "+", numY, "=", numX + numY)
+print(numX, "-", numY, "=", numX - numY)
+print(numX, "*", numY, "=", numX * numY)
+print(numX, "/", numY, "=", numX / numY)
