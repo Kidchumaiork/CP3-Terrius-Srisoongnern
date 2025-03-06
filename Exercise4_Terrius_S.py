e8487927be7ccd64c5575430301831b45ca9e07c
Kidchumaiork <glaosad5@gmail.com>
2025-03-06 16:25:27 +0700

    Submit Exercise4
    Exercise4 BorntoDev

---
+++ Exercise4_Terrius_S.py
@@ -0,0 +1,6 @@
+subject1 = input("Input score for Foundation English subject: ")
+subject2 = input("Input score for General Business subject: ")
+subject3 = input("Input score for Introduction to Computer Systems subject: ")
+subject4 = input("Input score for Computer Programming subject: ")
+
+print("--- Your Score ---", "\nFoundation English :", subject1, "\nGeneral Business :", subject2, "\nIntroduction to Computer Systems :", subject3, "\nComputer Programming :", subject4)
